92792c19d038e2345556f8cb0b61f4e4566fae8c
Monark Jain <jainmonark145@gmail.com>
2022-01-12 22:47:51 +0530

    fixed error of undefined

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,12 +7,23 @@ import { DataServiceService } from 'src/app/services/data-service.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
   constructor(private dataService: DataServiceService) {}
 
   ngOnInit(): void {
     this.dataService.getGlobalData().subscribe({
       next: (result) => {
         console.log(result);
+
+        result.forEach(cs=>{
+          this.totalActive+=cs.active||1
+          this.totalConfirmed+=cs.confirmed||1
+          this.totalDeaths+=cs.deaths||1
+          this.totalRecovered+=cs.recovered||1
+        })
       },
     });
   }
--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -7,27 +7,39 @@ import { GlobalDataSummary } from '../models/global-data';
   providedIn: 'root',
 })
 export class DataServiceService {
-  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/12-19-2021.csv`;
+  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-12-2021.csv`;
   constructor(private http: HttpClient) {}
 
   getGlobalData() {
     return this.http.get(this.globalDataUrl, { responseType: 'text' }).pipe(
       map((result) => {
         let data: GlobalDataSummary[] = [];
+        let raw: any = {};
         let rows = result.split('\n');
+        rows.splice(0, 1);
         rows.forEach((row) => {
           let cols = row.split(/,(?=\S)/);
-          data.push({
+
+          let cs:any = {
             country: cols[3],
             confirmed: +cols[7],
             deaths: +cols[8],
             recovered: +cols[9],
             active: +cols[10],
-          });
+          };
+          let temp: GlobalDataSummary = raw[cs.country];
+          if (temp) {
+            temp.active = cs.active + temp.active;
+            temp.confirmed = cs.confirmed + temp.confirmed;
+            temp.deaths = cs.active + temp.deaths;
+            temp.recovered = cs.active + temp.recovered;
+            raw[cs.country] = temp;
+          } else {
+            raw[cs.country] = cs;
+          }
         });
-        console.log(data);
 
-        return [];
+        return <GlobalDataSummary[]>Object.values(raw);
       })
     );
   }
